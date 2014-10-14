cb64d9636421061aa3ae7d3c8aa162af99e8142e
Beverly Lau <beverly.a.lau@gmail.com>
2014-10-14 11:41:57 -0700

    [BL] Using firebase to persist

--- app/scripts/services/posts.js
+++ app/scripts/services/posts.js
@@ -1,3 +1,3 @@
 app.factory('Post', function($resource){
-	return $resource('https://dazzling-torch-7191.firebaseio.com/posts.json');
+	return $resource('https://dazzling-torch-7191.firebaseio.com/posts/:id.json');
 })
